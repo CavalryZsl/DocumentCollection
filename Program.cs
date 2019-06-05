9b1ebcc744fefaf51bd25cc61b4daf764d55fb28
zhengshulei <zhengshulei@hotmail.com>
2019-06-05 19:10:46 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DocumentCollection.Log;
+using DocumentCollectionData;
+
+
+namespace DocumentCollection
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Test";
+            AppDomain.CurrentDomain.UnhandledException += (sender, eventargs) =>
+            {
+                Exception e = (Exception)eventargs.ExceptionObject;
+                Logger.Info(e);
+            };
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
+
+            DataProcess dataProcess = new DataProcess( @"H:\各工序文件获取格式归类", new string[] { });
+
+            var dt = dataProcess.Collection();
+
+            Console.Read();
+        }
+    }
+}
